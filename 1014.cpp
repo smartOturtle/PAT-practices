cf39a04741caf073cd6802abeec35d64a89f5f44
smartOturtle <734991706@qq.com>
2017-12-31 18:17:19 +0800

    Update 1014.cpp

--- 1014.cpp
+++ 1014.cpp
@@ -1,59 +1,50 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
 #include <functional>
 using namespace std;
 vector<int> serveTime;
 int windowSize, windowCapacity, customerSize, querySize;
 vector<int> queryTable;
-//时间驱动
 void Process()
 {
 	int currentTime = 480, customerIdx = 0;
 	queryTable.assign(customerSize, -1);
 	vector<deque<int>> windows(windowSize);
 	for (int i = 0; i < windowCapacity; ++i)
-		for (auto && window : windows)	
+	{
+		for (auto && window : windows)
+		{
 			window.push_back(customerIdx++);
-	vector<pair<int, int>> serveQueue;
+			if (customerIdx == customerSize)break;
+		}
+		if (customerIdx == customerSize)break;
+	}
+	priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int, int>>> heap;
 	for (int i = 0; i < windowSize; ++i)
 	{
 		if (!windows[i].empty())
-			serveQueue.emplace_back(serveTime[windows[i].front()], i);
+			heap.push({ serveTime[windows[i].front()] + currentTime, i });
 	}
-	using Pred = greater<pair<int, int>>;
-	make_heap(serveQueue.begin(), serveQueue.end(), Pred());
-	while (!serveQueue.empty())
+	while (!heap.empty())
 	{
-		auto completeCust = serveQueue.front();
-		auto& completeWindow = windows[completeCust.second];
-		//record and update 
-		queryTable[completeWindow.front()] = (currentTime += completeCust.first);
-		for (auto && customer : serveQueue)
-		{
-			customer.first -= completeCust.first;
-		}
-		//pop
-		pop_heap(serveQueue.begin(), serveQueue.end(), Pred());
-		serveQueue.pop_back();
-		completeWindow.pop_front();
-		//push
+		auto completeCust = heap.top();
+		auto& completedWindow = windows[completeCust.second];
+		currentTime = completeCust.first;
+		queryTable[completedWindow.front()] = currentTime;
+		heap.pop();
+		completedWindow.pop_front();
 		if (currentTime<17 * 60)
 		{
 			if (customerIdx<customerSize)
-				completeWindow.push_back(customerIdx++);
-			if (!completeWindow.empty())
-			{
-				serveQueue.emplace_back(serveTime[completeWindow.front()], completeCust.second);
-				push_heap(serveQueue.begin(), serveQueue.end(), Pred());
-			}
+				completedWindow.push_back(customerIdx++);
+			if (!completedWindow.empty())
+				heap.push({ serveTime[completedWindow.front()] + currentTime, completeCust.second });
 		}
 	}
 }
 int main()
 {
-	//serveTime 8:00~17:00
 	cin >> windowSize >> windowCapacity >> customerSize >> querySize;
 	serveTime.resize(customerSize);
 	for (int i = 0; i < customerSize; ++i)
