43c689b3123044262b06ab12e3c2dc60e14c26a7
smartOturtle <734991706@qq.com>
2018-01-12 17:21:22 +0800

    Create 1023.cpp

---
+++ 1023.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <string>
+#include <list>
+using namespace std;
+int main()
+{
+	string s;
+	cin >> s;
+	unordered_map<int, int> check;
+	list<int> l{0};
+	for (auto c : s)
+	{
+		int digit = c - '0';
+		if (check.find(digit) == check.end())check[digit] = 1;
+		else { ++check[digit]; }
+	}
+	for(auto i=s.rbegin();i!=s.rend();++i)
+	{
+		int digit = (*i - '0') * 2;
+		l.front() += digit % 10;
+		l.push_front(digit / 10);
+	}
+	if (l.front() == 0)l.pop_front();
+	if (s.size() != l.size())cout << "No\n";
+	else
+	{
+		for (auto value : l)
+		{
+			check[value]--;
+		}
+		auto right= all_of(check.begin(), check.end(), [](const pair<int, int>& a) { return a.second == 0; });
+		if (right)cout << "Yes\n";
+		else cout << "No\n";
+	}
+	for (auto value : l)
+	{
+		cout << value;
+	}
+}
