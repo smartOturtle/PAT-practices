b436af84c50f2aef56931f7133d1afb77e906ac0
smartOturtle <734991706@qq.com>
2018-02-28 23:44:10 +0800

    Update 1060.cpp

--- 1060.cpp
+++ 1060.cpp
@@ -1,22 +1,15 @@
 #define  _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 int Magnititudes(const string& num)
 {
-	if (num == "0")return 0;
-	if (num.find("0.")!=string::npos)
-	{
-		int pos = num.substr(2).find_first_not_of('0');
-		if (pos == string::npos)return 0;
-		else return -pos;
-	}
-	else
-	{
-		int last = num.find('.');
-		if (last == string::npos)last = num.size();
-		return last - num.find_first_not_of('0');
-	}
+	auto dotPos = find(num.begin(),num.end(),'.');
+	auto firstNotZeroPos = find_if(num.begin(), num.end(), [](char c) { return c != '0'&&c!='.'; });
+	if (firstNotZeroPos==num.end()) return 0;
+	if (firstNotZeroPos < dotPos) return dotPos - firstNotZeroPos;
+	return dotPos - firstNotZeroPos + 1;
 }
 string ToScientificNotation(const string& num, int digitSize)
 {
@@ -25,7 +18,7 @@ string ToScientificNotation(const string& num, int digitSize)
 	int magnititudes = Magnititudes(num);
 	int cnt = 0;
 	int begin = num.find_first_not_of('0');
-	if(magnititudes<0)begin = -magnititudes + 2;
+	if(magnititudes<0)begin = -magnititudes + begin+1;
 	for (int i = begin;; i++)
 	{
 		if (i >= num.size())s.push_back('0');
