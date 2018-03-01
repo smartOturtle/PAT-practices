799ce379496ec7ba1655b5c493c1b79541f014b7
smartOturtle <734991706@qq.com>
2018-03-01 19:36:29 +0800

    Update 1060.cpp

--- 1060.cpp
+++ 1060.cpp
@@ -5,32 +5,25 @@
 using namespace std;
 int Magnititudes(const string& num)
 {
+	auto dotPos = find(num.begin(),num.end(),'.');
 	auto firstNotZeroPos = find_if(num.begin(), num.end(), [](char c) { return c != '0'&&c!='.'; });
 	if (firstNotZeroPos==num.end()) return 0;
-	auto dotPos = find(num.begin(),num.end(),'.');
 	if (firstNotZeroPos < dotPos) return dotPos - firstNotZeroPos;
 	return dotPos - firstNotZeroPos + 1;
 }
 string ToScientificNotation(const string& num, int digitSize)
 {
-	string s;
-	s += "0.";
-	int magnititudes = Magnititudes(num);
+	string s{"0."};
 	int cnt = 0;
-	int begin = num.find_first_not_of('0');
-	if(magnititudes<0)begin = -magnititudes + begin+1;
-	for (int i = begin;; i++)
+	auto iter= find_if(num.begin(), num.end(), [](char c) { return c != '0'&&c != '.'; });
+	for(;iter!=num.end();++iter)
 	{
-		if (i >= num.size())s.push_back('0');
-		else
-		{
-			if (num[i] == '.')continue;
-			s.push_back(num[i]);
-		}
-		if (++cnt == digitSize)break;
+		if(*iter=='.')continue;
+		s.push_back(*iter);
+		if(++cnt>=digitSize)break;
 	}
-	s.push_back('*');
-	s += "10^" + to_string(magnititudes);
+	if(digitSize>cnt)s += string(digitSize - cnt, '0');
+	s += "*10^" + to_string(Magnititudes(num));
 	return s;
 }
 int main(int argc, char *argv[])
