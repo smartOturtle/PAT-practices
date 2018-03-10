df91469b1802538b8b3b03abbbbe2174ab9c6ae9
smartOturtle <734991706@qq.com>
2018-03-11 01:19:40 +0800

    Update 1072.cpp

--- 1072.cpp
+++ 1072.cpp
@@ -3,81 +3,73 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <numeric>
+#include <deque>
 using namespace std;
-int N, M, K, D;
+int houseSize, candidateSize, roadSize, maxServiceDis;
 vector<vector<int> > gragh;
-pair<double, double> dijkstra(int s)
+void Dijkstra(int idx)
 {
-	vector<int> dis(N + M + 1, INT32_MAX);
-	vector<bool> used(N + M + 1, false);
-	dis[s] = 0;
+	auto& dis=gragh[idx];
+	deque<bool> visited(houseSize + candidateSize, false);
+	dis[idx] = 0;
 	while (true)
 	{
 		int v = -1;
-		for (int u = 1; u <= N + M; u++)
+		for (int i =0; i < houseSize + candidateSize; i++)
 		{
-			if (!used[u] && (v == -1 || dis[u]<dis[v])) v = u;
+			if (!visited[i]&&dis[i]<INT16_MAX && (v == -1 || dis[i]<dis[v])) v = i;
 		}
-		if (v == -1) break;
-		used[v] = true;
-		for (int u = 1; u <= N + M; u++)
+		if (v == -1) return;
+		visited[v] = true;
+		for (int i = 0; i <houseSize + candidateSize; i++)
 		{
-			dis[u] = min(dis[u], dis[v] + gragh[v][u]);
+			if(!visited[i])dis[i] = min(dis[i], dis[v] + gragh[v][i]);
 		}
 	}
-	double min = INT32_MAX, cost = 0;
-	for (int i = 1; i <= N; i++)
-	{
-		if (dis[i] > D)
-		{
-			return make_pair(-1, 0);
-		}
-		if (dis[i]<min)
-		{
-			min = dis[i];
-		}
-		cost += dis[i];
-	}
-	return make_pair(min, cost / N);
 }
 
+int ToIndex(const string& str)
+{
+	if (isalpha(str[0]))	return  stoi(str.substr(1)) + houseSize - 1;
+	return  stoi(str)-1;
+}
 int main()
 {
-	scanf("%d%d%d%d", &N, &M, &K, &D);
-	int from, to, dis, i;
-	gragh.resize(N + M + 1, vector<int>(N + M + 1, INT32_MAX));
-	string str;
-	for (i = 0; i<K; i++)
+	scanf("%d%d%d%d", &houseSize, &candidateSize, &roadSize, &maxServiceDis);
+	gragh.resize(houseSize + candidateSize, vector<int>(houseSize + candidateSize, INT16_MAX));
+	for (int i = 0; i<roadSize; i++)
 	{
+		string str;
 		cin >> str;
-		if (isalpha(str[0]))	from = stoi(str.substr(1)) + N;
-		else	from = stoi(str);
-
+		int from = ToIndex(str);
 		cin >> str;
-		if (isalpha(str[0]))	to = stoi(str.substr(1)) + N;
-		else	to = stoi(str);
+		int to = ToIndex(str);
+		int dis;
 		scanf("%d", &dis);
 		gragh[from][to] = dis;
 		gragh[to][from] = dis;
 	}
 	int index = -1;
-	double ave=0, min = -1;
-	for (i = N + 1; i <= N + M; i++)
+	double sum=0, minDis = -1;
+	for (int i = houseSize ; i < houseSize + candidateSize; i++)
 	{
-		pair<double, double> p = dijkstra(i);
-		if (p.first <= -1)continue;
-		if (p.first>min || (p.first == min && p.second<ave))
+		Dijkstra(i);
+		auto& eachDis = gragh[i];
+		auto minMax = minmax_element(eachDis.begin(), eachDis.begin() + houseSize);
+		if(*minMax.second>maxServiceDis)continue;
+		double tempSum = accumulate(eachDis.begin(), eachDis.begin() + houseSize, 0);
+		if(minDis<*minMax.first|| *minMax.first == minDis&&tempSum<sum)
 		{
 			index = i;
-			min = p.first;
-			ave = p.second;
+			sum = tempSum;
+			minDis = *minMax.first;
 		}
 	}
 	if (index == -1)printf("No Solution\n");
 	else
 	{
-		printf("G%d\n", index - N);
-		printf("%.1f %.1f\n", min, ave);
+		printf("G%d\n", index - houseSize+1);
+		printf("%.1f %.1f\n", minDis, sum/houseSize);
 	}
-	return 0;
 }
