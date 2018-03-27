1f00d5d8d84e167c20d1b35bb46d33ccd721912d
smartOturtle <734991706@qq.com>
2018-03-27 23:14:29 +0800

    Update 1082.cpp

--- 1082.cpp
+++ 1082.cpp
@@ -12,7 +12,7 @@ unordered_map<char, string> digitStringMap
 	{'0',"ling"},{'1',"yi"},{'2',"er"},{'3',"san"},{'4',"si"},{'5',"wu"},
 	{'6',"liu"},{'7',"qi"},{'8',"ba"},{'9',"jiu"},
 };
-unordered_map<int, string> manititudeStringMap
+unordered_map<int, string> magnititudeStringMap
 {
 	{ 1,"Shi" },{ 2,"Bai" },{ 3,"Qian" },{ 4,"Wan" },
 	{ 5,"Shi" },{ 6,"Bai" },{ 7,"Qian" },{ 8,"Yi" },
@@ -40,15 +40,15 @@ int main(int argc, char* argv[])
 			{
 				s.push_front(digitStringMap['0']);
 			}
-			if(manititudeStringMap.find(i)!=manititudeStringMap.end())
+			if(magnititudeStringMap.find(i)!=magnititudeStringMap.end())
 			{
 				if (i == 4)hasSetWan = true;
-				else if(i>4&&i<8&&!hasSetWan)
+				else if(i>=4&&i<8&&!hasSetWan)
 				{
-					s.push_front(manititudeStringMap[4]);
+					s.push_front(magnititudeStringMap[4]);
 					hasSetWan = true;
 				}
-				s.push_front(manititudeStringMap[i]);
+				s.push_front(magnititudeStringMap[i]);
 			}
 			s.push_front(digitStringMap[num[i]]);
 		}
