a052ec61b8b6734db10eb59b1b7c33b3dd246a54
wumk-nullptr <734991706@qq.com>
2018-04-09 19:58:24 +0800

    Create 1098.cpp

---
+++ 1098.cpp
@@ -0,0 +1,58 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <array>
+#include <map>
+#include <algorithm>
+using namespace std;
+template<typename Iter>
+void InsertionSort(Iter begin, Iter end)
+{
+    for (auto i = begin; i != end; ++i)
+    {
+        auto j = i;
+        auto temp = *i;
+        for (; j != begin && *(j - 1) > temp; --j)
+            *j = *(j - 1);
+        *j = temp;
+    }
+}
+int main(int argc, char* argv[])
+{
+    int N;
+    cin >> N;
+    vector<int> seq(N);
+    vector<int> after(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> seq[i];
+    }
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> after[i];
+    }
+    auto sortedEnd = is_sorted_until(after.begin(), after.end());
+    bool isInsertionSort = true;
+    for (auto i = sortedEnd - after.begin(); i<after.size(); ++i)
+    {
+        if (seq[i] != after[i])isInsertionSort = false;
+    }
+    if (isInsertionSort)
+    {
+        cout << "Insertion Sort\n";
+        InsertionSort(after.begin(), sortedEnd + 1);
+    }
+    else
+    {
+        cout << "Heap Sort\n";
+        auto heapEnd = is_heap_until(after.begin(), after.end());
+        pop_heap(after.begin(), heapEnd);
+    }
+    cout << after.front();
+    for (int i = 1; i < after.size(); ++i)
+    {
+        cout << " " << after[i];
+    }
+}
