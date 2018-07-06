eada6ec06425a653934cc9d9f33075fd4ed59e64
wumk <734991706@qq.com>
2018-07-07 01:30:11 +0800

    Update 1004.cpp
    level order travelsal

--- 1004.cpp
+++ 1004.cpp
@@ -1,60 +1,55 @@
 #include <iostream>
-#include <queue>
+#include <algorithm>
+#include <vector>
+#include <string>
 #include <unordered_map>
+#include <queue>
 using namespace std;
-unordered_map<int, vector<int>> nodes;
-void LevelOrderTraversal()
-{
-  queue<int> q;
-  auto& root = nodes[01];
-  if (root.empty())
-  {
-    cout << 1;
-    return;
-  }
-  for (auto && child : root)
-    q.push(child);
-  int cnt = 0, last = root.size(), leafSize = 0;
-  cout << 0;
-  for (int i = 0; !q.empty(); q.pop())
-  {
-    auto& children = nodes[q.front()];
-    if (children.empty())leafSize++;
-    else
-    {
-      for (auto child : children)
-        q.push(child);
-      cnt += children.size();
-    }
-    if (++i == last)
-    {
-      last = cnt;
-      cnt = 0;
-      i = 0;
-      cout << ' ' << leafSize;
-      leafSize = 0;
-    }
-  }
-}
-void Input()
-{
-  int parentId, childrenSize;
-  cin >> parentId >> childrenSize;
-  if (nodes.find(parentId) == nodes.end())
-    nodes.insert({ parentId,{} });
-  for (int i = 0; i < childrenSize; ++i)
-  {
-    int id;
-    cin >> id;
-    if(nodes.find(id)==nodes.end())
-      nodes.insert({ id,{} });
-    nodes[parentId].push_back(id);
-  }
-}
+
 int main()
 {
-  int totalSize, nonLeafSize;
-  cin >> totalSize >> nonLeafSize;
-  for (int i = 0; i < nonLeafSize; ++i) Input();
-  LevelOrderTraversal();
+    unordered_map<int, vector<int>> tree;
+    int N, M;
+    cin >> N >> M;
+    for (int i = 0; i < M; ++i)
+    {
+        int id;
+        cin >> id;
+        int K;
+        cin >> K;
+        for (int j = 0; j < K; ++j)
+        {
+            int childId;
+            cin >> childId;
+            tree[id] .push_back(childId) ;
+        }
+    }
+    queue<int> q;
+    q.push(01);
+    int levelLeaveSize = 0;
+    int levelSize = 1;
+    int levelCount = 0;
+    vector<int> eachLevelLeaveSize;
+    while (!q.empty())
+    {
+        if(tree[q.front()].empty())
+        {
+            levelLeaveSize++;
+        }
+        else for (auto id : tree[q.front()])q.push(id);
+        q.pop();
+        ++levelCount;
+        if(levelCount==levelSize)
+        {
+            levelCount = 0;
+            levelSize = q.size();
+            eachLevelLeaveSize.push_back(levelLeaveSize);
+            levelLeaveSize = 0;
+        }
+    }
+    cout << eachLevelLeaveSize.front();
+    for (int i = 1; i < eachLevelLeaveSize.size(); ++i)
+    {
+        cout << " " << eachLevelLeaveSize[i];
+    }
 }
