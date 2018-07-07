22536945f14b6de886abbd43c496700afa216018
wumk <734991706@qq.com>
2018-07-08 00:51:28 +0800

    Update 1005.cpp

--- 1005.cpp
+++ 1005.cpp
@@ -1,21 +1,3 @@
-#include <iostream>
-#include <string>
-#include <cmath>
-using namespace std;
-int main()
-{
-  string s;
-  cin >> s;
-  int result=0;
-  for (auto && value : s)
-    result += value-'0';
-  string spell[]{"zero","one","two","three","four","five","six","seven","eight","nine"};
-  int i;
-  for (i = 1; result / static_cast<int>(pow(10, i)); ++i){}
-  for (i--; i >0; --i)
-  {
-    cout << spell[result / static_cast<int>(pow(10, i))]<<" ";
-    result %= static_cast<int>(pow(10, i));
-  }
-  cout << spell[result];
-}
+pronunciations = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 
+                  5: 'five',6: 'six', 7: 'seven', 8: 'eight',9: 'nine'}
+print(*(pronunciations[int(i)] for i in str(sum(int(i) for i in input()))))
