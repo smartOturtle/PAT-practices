8db3bc7d86b1f9973e9b4fa1173f57437a4e7589
wumk <734991706@qq.com>
2018-07-19 00:56:39 +0800

    Update and rename 1013.cpp to 1013 Battle Over Cities.cpp
    union find & must use scanf

---
+++ 1013 Battle Over Cities.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct UnionFindSet
+{
+    unordered_map<int,int> container;
+    UnionFindSet(int size,int except)
+    {
+        for (int i = 1; i <= size; ++i)if (i != except)container[i] = -1;
+    }
+    int Find(int idx)
+    {
+        if (container[idx] < 0)return idx;
+        return container[idx] = Find(container[idx]);
+    }
+    void Union(int lhs,int rhs)
+    {
+        int lhsRoot = Find(lhs);
+        int rhsRoot = Find(rhs);
+        if(lhsRoot!=rhsRoot)
+        {
+            if (lhsRoot < rhsRoot)swap(lhsRoot, rhsRoot);
+            container[rhsRoot] += container[lhsRoot];
+            container[lhsRoot] = rhsRoot;
+        }
+    }
+};
+int main(int argc, char* argv[])
+{
+    int citySize, roadSize, checkSize;
+    cin >> citySize >> roadSize >> checkSize;
+    vector<pair<int, int>> edges(roadSize);
+    for (auto& edge : edges)scanf("%d%d",&edge.first,&edge.second);
+    for (int i = 0; i < checkSize; ++i)
+    {
+        int except;
+        cin >> except;
+        UnionFindSet ufset(citySize, except);
+        for_each(edges.begin(), edges.end(),[&](pair<int,int> edge)
+        {
+            if (edge.first != except && edge.second != except)ufset.Union(edge.first, edge.second);
+        });
+        cout << count_if(ufset.container.begin(), ufset.container.end(), [](pair<int, int> item) { return item.second < 0; })-1<<'\n';
+    }
+}
+
