c0bc61f3dfc263ce855c38335a765b1a52506c10
wumk <734991706@qq.com>
2018-07-24 01:55:11 +0800

    Update 1012 The Best Rank.py

--- 1012 The Best Rank.py
+++ 1012 The Best Rank.py
@@ -1,21 +1,26 @@
+from functools import *
+from collections import defaultdict
+
 student_size, query_size = [int(i) for i in input().split(' ')]
 students = {}
 for i in range(student_size):
-    student = [int(value) for value in input().split(' ')]
-    students[student[0]] = [sum(student[1:]) / 3]
-    students[student[0]].extend(student[1:])
+    student_id, *scores = (int(value) for value in input().split(' '))
+    students[student_id] = [sum(scores) / 3]
+    students[student_id].extend(scores)
+
+
+def build_rank(rank, score_group, course_id):
+    global students
+    for student_id in score_group[1]:
+        students[student_id][course_id] = rank
+    return rank + len(score_group[1])
+
+
 for i in range(4):
-    previous_score = -1
-    rank = 1
-    same_score = 0
-    for id,each_scores in sorted(students.items(), key=lambda s: s[1][i], reverse=True):
-        if each_scores[i] != previous_score:
-            rank += same_score
-            same_score = 1
-            previous_score = each_scores[i]
-        else:
-            same_score += 1
-        students[id][i] = rank
+    score_groups = defaultdict(list)
+    for student_id, scores in students.items():
+        score_groups[scores[i]].append(student_id)
+    reduce(partial(build_rank, course_id=i), sorted(score_groups.items(), reverse=True), 1)
 for i in range(query_size):
     student_id = int(input())
     if student_id in students:
