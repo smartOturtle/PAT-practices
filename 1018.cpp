245573a7e219e626e2778aeda7f1ef934c783db1
wumk <734991706@qq.com>
2018-07-23 15:56:05 +0800

    Update 1018.cpp

--- 1018.cpp
+++ 1018.cpp
@@ -1,91 +1,76 @@
 #include <iostream>
 #include <vector>
 #include <deque>
+#include <tuple>
 using namespace std;
-int capacity, n, sp, roadSize;
-vector<vector<int>> stations, pre;
-deque<int> weight, path, temppath,dist;
+int capacity, n, endIdx, roadSize;
+vector<vector<int>> gragh, pre;
+deque<int> stationWeights, path, temppath, dist;
 int minNeed = INT16_MAX, minBack = INT16_MAX;
-void dfs(int v)
+void DFS(int v)
 {
-  temppath.push_front(v);
-  for (auto && value : pre[v])
-    dfs(value);
-  if (pre[v].empty())
-  {
-    int need = 0, back = 0;
-    for (auto && idx : temppath)
+    temppath.push_front(v);
+    for (auto value : pre[v])DFS(value);
+    if (pre[v].empty())
     {
-      back += weight[idx];
-      if (back<0)
-      {
-        need += -back;
-        back = 0;
-      }
+        int need = 0, back = 0;
+        for (auto && idx : temppath)
+        {
+            back += stationWeights[idx];
+            if (back<0)
+            {
+                need += -back;
+                back = 0;
+            }
+        }
+        if (need < minNeed || (need == minNeed && minBack>back))
+            tie(path, minNeed, minBack) = { temppath,need,back };
     }
-    if (need < minNeed)
-    {
-      path = temppath;
-      minNeed = need;
-      minBack = back;
-    }
-    else if (need == minNeed && minBack>back)
-    {
-      path = temppath;
-      minBack = back;
-    }
-  }
-  temppath.pop_front();
+    temppath.pop_front();
 }
 int main()
 {
-  cin >> capacity >> n >> sp >> roadSize;
-  stations.assign(n + 1, vector<int>(n + 1, INT16_MAX));
-  dist.resize(stations.size(), INT16_MAX);
-  weight.resize(stations.size());
-  for (int i = 1; i < stations.size(); ++i)
-  {
-    cin >> weight[i];
-    weight[i] -= capacity / 2;
-  }
-  for (int i = 0; i < roadSize; ++i)
-  {
-    int start, end, time;
-    cin >> start >> end >> time;
-    stations[start][end] = time;
-    stations[end][start] = time;
-  }
-  pre.assign(stations.size(), {});
-  vector<bool> collected(stations.size());
-  auto& dist = stations[0];
-  dist[0] = 0;
-  while (true)
-  {
-    int idx = -1;
-    for (int i = 0; i < stations.size(); ++i)
+    cin >> capacity >> n >> endIdx >> roadSize;
+    gragh.assign(n + 1, vector<int>(n + 1, INT16_MAX));
+    dist.resize(gragh.size(), INT16_MAX);
+    stationWeights.resize(gragh.size());
+    for (int i = 1; i < gragh.size(); ++i)
     {
-      if (!collected[i] && dist[i] != INT16_MAX)
-        if (idx < 0 || dist[i] < dist[idx])idx = i;
+        cin >> stationWeights[i];
+        stationWeights[i] -= capacity / 2;
     }
-    if (idx == -1)break;
-    collected[idx] = true;
-    for (int i = 0; i < stations.size(); ++i)
+    for (int i = 0; i < roadSize; ++i)
     {
-      auto& edge = stations[idx][i];
-      if (edge != INT16_MAX && !collected[i])
-      {
-        if (dist[i]>dist[idx] + edge)
+        int start, end, time;
+        cin >> start >> end >> time;
+        gragh[start][end] = time;
+        gragh[end][start] = time;
+    }
+    pre.assign(gragh.size(), {});
+    deque<bool> collected(gragh.size());
+    auto dist = gragh[0];
+    dist[0] = 0;
+    while (true)
+    {
+        int idx = -1;
+        int min = INT16_MAX;
+        for (int i = 0; i < gragh.size(); ++i)
+            if (!collected[i] && dist[i] < min)tie(idx, min) = { i,dist[i] };
+        if (idx == -1)break;
+        collected[idx] = true;
+        for (int i = 0; i < gragh.size(); ++i)
         {
-          dist[i] = dist[idx] + edge;
-          pre[i].assign({ idx });
+            auto weight = gragh[idx][i];
+            if (dist[i]>dist[idx] + weight)
+            {
+                dist[i] = dist[idx] + weight;
+                pre[i].assign({ idx });
+            }
+            else if (dist[i] == dist[idx] + weight)pre[i].push_back(idx);
         }
-        else if (dist[i] == dist[idx] + edge)pre[i].push_back(idx);
-      }
     }
-  }
-  dfs(sp);
-  printf("%d 0", minNeed);
-  for (int i = 1; i < path.size(); i++)
-    printf("->%d", path[i]);
-  printf(" %d", minBack);
+    DFS(endIdx);
+    printf("%d 0", minNeed);
+    for (int i = 1; i < path.size(); i++)printf("->%d", path[i]);
+    printf(" %d", minBack);
 }
