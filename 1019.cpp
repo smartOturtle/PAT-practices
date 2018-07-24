e8d184bd7cfbe9dfa211c54db9ad68e995893690
wumk <734991706@qq.com>
2018-07-24 15:14:16 +0800

    Update 1019.cpp

--- 1019.cpp
+++ 1019.cpp
@@ -1,34 +1,25 @@
 #include <iostream>
 #include <string>
 #include <deque>
+#include <algorithm>
 using namespace std;
-bool IsPalindromic(const deque<int>& s)
-{
-	int middle = s.size() / 2,offset=0;
-	if (s.size() % 2 == 0)offset = 1;
-	for (int i = 0; middle + i <s.size(); ++i)
-	{
-		if (s[middle - i-offset] != s[middle+ i]) { return false; }
-	}
-	return true;
-}
 int main()
 {
-	int num;
-	int radix;
-	cin >> num >> radix;
-	deque<int> result;
-	while (num)
-	{
-		result.push_front(num%radix);
-		num /= radix;
-	}
-	if(IsPalindromic(result)) cout<<"Yes";
-  	else cout<<"No";
-	cout << "\n";
-	cout << result.front();
-	for (int i = 1; i < result.size(); ++i)
-	{
-		cout <<" "<< result[i];
-	}
+    int num;
+    int radix;
+    cin >> num >> radix;
+    deque<int> s;
+    while (num)
+    {
+        s.push_front(num%radix);
+        num /= radix;
+    }
+    if (mismatch(s.begin(), s.end(), s.rbegin(), s.rend()).first == s.end()) cout << "Yes\n";
+    else cout << "No\n";
+    if(!s.empty())
+    {
+        cout << s.front();
+        for (int i = 1; i < s.size(); ++i)cout << " " << s[i];
+    }
+    else cout << 0;
 }
