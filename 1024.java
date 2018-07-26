c89dbbf6bee63df5f80babf1337995f63542ea3e
wumk <734991706@qq.com>
2018-07-26 23:50:04 +0800

    Update 1024.java

--- 1024.java
+++ 1024.java
@@ -1,28 +1,9 @@
-import java.math.BigInteger;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args){
-        Scanner in=new Scanner(System.in);
-        String s=in.next();
-        BigInteger num=new BigInteger(s);
-        int maxStep=in.nextInt();
-        int step=0;
-        while(true){
-            boolean isPalindromic=true;
-            for (int i=0;i<s.length()/2;i++){
-                if(s.charAt(i)!=s.charAt(s.length()-1-i)){
-                    isPalindromic=false;
-                    break;
-                }
-            }
-            if(isPalindromic)break;
-            StringBuilder sb=new StringBuilder(s);
-            BigInteger reverse=new BigInteger(sb.reverse().toString());
-            num=num.add(reverse);
-            s=num.toString();
-            if(++step>=maxStep)break;
-        }
-        System.out.println(s);
-        System.out.println(step);
-    }
-}
+num, max_step = map(int, input().split())
+for i in range(max_step):
+    num_str = str(num)
+    if all(p[0] == p[1] for p in zip(num_str, reversed(num_str))):
+        print(num, i, sep='\n')
+        break
+    num += int(num_str[::-1])
+else:
+    print(num, max_step, sep='\n')
