dd3e202d292465c8786d824d6aa90a09edbd7db6
wumk <734991706@qq.com>
2018-07-26 23:38:49 +0800

    Update 1023.cpp

--- 1023.cpp
+++ 1023.cpp
@@ -1,40 +1,27 @@
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
 #include <string>
-#include <list>
+#include <algorithm>
 using namespace std;
-int main()
+
+int main(int argc, char* argv[])
 {
-	string s;
-	cin >> s;
-	unordered_map<int, int> check;
-	list<int> l{0};
-	for (auto c : s)
-	{
-		int digit = c - '0';
-		++check[digit];
-	}
-	for(auto i=s.rbegin();i!=s.rend();++i)
-	{
-		int digit = (*i - '0') * 2;
-		l.front() += digit % 10;
-		l.push_front(digit / 10);
-	}
-	if (l.front() == 0)l.pop_front();
-	if (s.size() != l.size())cout << "No\n";
-	else
-	{
-		for (auto value : l)
-		{
-			check[value]--;
-		}
-		auto right= all_of(check.begin(), check.end(), [](const pair<int, int>& a) { return a.second == 0; });
-		if (right)cout << "Yes\n";
-		else cout << "No\n";
-	}
-	for (auto value : l)
-	{
-		cout << value;
-	}
+    string a;
+    cin >> a;
+    auto b = a;
+    bool carry = false;
+    for (int i = a.size()-1; i >=0; --i)
+    {
+        int c = (a[i] - '0')*2+carry;
+        if (c >= 10)
+        {
+            carry = true;
+            c %= 10;
+        }
+        else { carry = false; }
+        a[i] = c + '0';
+    }
+    if (carry)a.insert(a.begin(), '1');
+    if (is_permutation(a.begin(), a.end(), b.begin(), b.end()))cout << "Yes";
+    else { cout << "No"; }
+    cout << '\n' << a;
 }
