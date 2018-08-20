7f911a4350585eed3d0a669dfcb895e6ff119837
wumk <734991706@qq.com>
2018-08-21 00:57:52 +0800

    Update 1060.cpp

--- 1060.cpp
+++ 1060.cpp
@@ -5,34 +5,33 @@
 using namespace std;
 int Magnititudes(const string& num)
 {
-	auto firstNotZeroPos = find_if(num.begin(), num.end(), [](char c) { return c != '0'&&c!='.'; });
-	if (firstNotZeroPos==num.end()) return 0;
-	auto dotPos = find(num.begin(), num.end(), '.');
-	if (firstNotZeroPos < dotPos) return dotPos - firstNotZeroPos;
-	return dotPos - firstNotZeroPos + 1;
+    auto firstNotZeroPos = find_if(num.begin(), num.end(), [](char c) { return c != '0'&&c != '.'; });
+    if (firstNotZeroPos == num.end()) return 0;
+    auto dotPos = find(num.begin(), num.end(), '.');
+    if (firstNotZeroPos < dotPos) return dotPos - firstNotZeroPos;
+    return dotPos - firstNotZeroPos + 1;
 }
 string ToScientificNotation(const string& num, int digitSize)
 {
-	string s{"0."};
-	int cnt = 0;
-	auto iter= find_if(num.begin(), num.end(), [](char c) { return c != '0'&&c != '.'; });
-	for(;iter!=num.end();++iter)
-	{
-		if(*iter=='.')continue;
-		s.push_back(*iter);
-		if(++cnt>=digitSize)break;
-	}
-	string(digitSize - cnt, '0');
-	s += "*10^" + to_string(Magnititudes(num));
-	return s;
+    string s{ "0." };
+    int cnt = 0;
+    auto iter = find_if(num.begin(), num.end(), [](char c) { return c != '0'&&c != '.'; });
+    for (; iter != num.end(); ++iter)
+    {
+        if (*iter == '.')continue;
+        s.push_back(*iter);
+        if (++cnt >= digitSize)break;
+    };
+    s += string(digitSize - cnt, '0')+ "*10^" + to_string(Magnititudes(num));
+    return s;
 }
 int main(int argc, char *argv[])
 {
-	int digitSize;
-	string a, b;
-	cin >> digitSize >> a >> b;
-	a = ToScientificNotation(a, digitSize);
-	b = ToScientificNotation(b, digitSize);
-	if (a == b)cout << "YES " << a;
-	else cout << "NO " << a << " " << b;
+    int digitSize;
+    string a, b;
+    cin >> digitSize >> a >> b;
+    a = ToScientificNotation(a, digitSize);
+    b = ToScientificNotation(b, digitSize);
+    if (a == b)cout << "YES " << a;
+    else cout << "NO " << a << " " << b;
 }
