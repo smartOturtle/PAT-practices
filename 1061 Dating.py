21a622635ab36eec751a8336d138f741d0752b02
wumk <734991706@qq.com>
2018-08-21 02:56:37 +0800

    Update 1061 Dating.py

--- 1061 Dating.py
+++ 1061 Dating.py
@@ -5,15 +5,14 @@ hours = {k: v for k, v in zip(string.digits + string.ascii_uppercase, range(24))
 first_pair = [input(), input()]
 second_pair = [input(), input()]
 first_common = True
-for c1, c2 in zip(*first_pair):
-    if c1 == c2:
-        if first_common:
-            if c1 in weekdays:
-                weekday = weekdays[c1]
-                first_common = False
-        elif c1 in hours:
-            hour = hours[c1]
-            break
+for c, _ in filter(lambda p: p[0] == p[1], zip(*first_pair)):
+    if first_common:
+        if c in weekdays:
+            weekday = weekdays[c]
+            first_common = False
+    elif c in hours:
+        hour = hours[c]
+        break
 for i, (c1, c2) in enumerate(zip(*second_pair)):
     if str.isalpha(c1) and c1 == c2:
         minute = i
