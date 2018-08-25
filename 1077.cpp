b414a01a80bdf27bcf9697ec0b0436f7b7098ee9
wumk <734991706@qq.com>
2018-08-26 03:59:12 +0800

    Update 1077.cpp

--- 1077.cpp
+++ 1077.cpp
@@ -1,40 +1,29 @@
-#define  _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
-#include <string>
-#include <deque>
 #include <algorithm>
+#include <functional>
+#include <string>
 using namespace std;
+
 int main(int argc, char* argv[])
 {
-	int size;
-	cin >> size;
-	getchar();
-	vector<string> sentences;
-	for (int i = 0; i < size; ++i)
-	{
-		string s;
-		getline(cin, s);
-		sentences.push_back(s);
-	}
-	string suffix;
-	int reversedIdx = 1;
-	while (sentences[0].size()>=reversedIdx)
-	{
-		char c = *(sentences[0].end()-reversedIdx);
-		for (int i = 1; i < sentences.size(); ++i)
-		{
-			if (sentences[i].size() < reversedIdx || c != *(sentences[i].end() - reversedIdx))
-				goto out;
-		}
-		reversedIdx++;
-		suffix.push_back(c);
-	}
-	out:
-	if (suffix.empty())cout << "nai";
-	else
-	{
-		reverse(suffix.begin(), suffix.end());
-		cout << suffix;
-	}
+    int size;
+    cin >> size;
+    getchar();
+    vector<string> sentences(size);
+    for (auto&s : sentences)getline(cin, s);
+    auto shortestSize = min_element(sentences.begin(), sentences.end(), 
+        [](auto& lhs, auto& rhs) { return lhs.size() < rhs.size(); })->size();
+    string kuchiguse;
+    for (int i = 0; i < shortestSize; ++i)
+    {
+        if (all_of(sentences.begin(), sentences.end(), 
+            [&](const string& s) { return *(s.rbegin()+i)==*(sentences[0].rbegin()+i); }))
+        {
+            kuchiguse.push_back(*(sentences[0].rbegin()+ i));
+        }
+        else { break; }
+    }
+    reverse(kuchiguse.begin(), kuchiguse.end());
+    cout << (kuchiguse.empty() ? "nai" : kuchiguse);
 }
